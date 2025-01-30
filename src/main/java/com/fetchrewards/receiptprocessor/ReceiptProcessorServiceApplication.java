ae82c11a3e3d3e8d0f86d220612027b155f868ed
AakashRajCh <96636398+AakashRajCh@users.noreply.github.com>
2025-01-29 23:50:00 -0600

    Initial commit of Receipt Processor project

---
+++ src/main/java/com/fetchrewards/receiptprocessor/ReceiptProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fetchrewards.receiptprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptProcessorServiceApplication.class, args);
+	}
+
+}
